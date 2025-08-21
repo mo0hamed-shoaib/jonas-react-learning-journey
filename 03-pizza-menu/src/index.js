e8b34640d963ccd363f2ec3c04784a43a56ca278
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-08-21 09:12:25 +0300

    basic react function

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,2 +1,18 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>This is ReactJS</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
