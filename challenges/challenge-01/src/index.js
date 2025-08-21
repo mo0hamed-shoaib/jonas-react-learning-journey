0819deeb05225490df472b546b005540d52b6ca5
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-08-21 11:09:30 +0300

    initial setup for challenge 1 - profile card

---
+++ challenges/challenge-01/src/index.js
@@ -0,0 +1,13 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return <h1>Hello</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
