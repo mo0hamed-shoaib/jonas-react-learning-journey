07ea691c53de262f27fbe8092959e24bba652c52
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-09-09 04:14:01 +0300

    setup the steps mini project static look

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -1,5 +1,33 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 function App() {
-  return <h1>Hello Steps</h1>;
+  const step = 1;
+
+  return (
+    <div className="steps">
+      <div className="numbers">
+        {/* we used >= not == because steps are progressive, and they are active along the way */}
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
