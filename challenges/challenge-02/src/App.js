d28bbebbcffb5a677354fa17cabb5b3efa7b39b2
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-09-16 22:49:11 +0300

    challenge 2 solution with cleaner approach

--- challenges/challenge-02/src/App.js
+++ challenges/challenge-02/src/App.js
@@ -1,78 +1,50 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
     <div>
-      <Steps />
+      <h3>Simple days Navigator</h3>
+      <Counter />
     </div>
   );
 }
 
-function Steps() {
+function Counter() {
   const [step, setStep] = useState(1);
-  function nextStep() {
-    setStep((s) => s + 1);
-  }
-
-  function prevStep() {
-    if (step > 1) setStep((s) => s - 1);
-  }
-
-  return (
-    <div>
-      <button onClick={prevStep}>-</button>
-      <span>Step: {step}</span>
-      <button onClick={nextStep}>+</button>
-      <Count step={step} />
-    </div>
-  );
-}
-
-function Count({ step }) {
   const [count, setCount] = useState(0);
 
-  function increaseCount() {
-    setCount((s) => s + step);
-  }
-
-  function decreaseCount() {
-    setCount((s) => s - step);
-  }
-
-  return (
-    <div>
-      <button onClick={decreaseCount}>-</button>
-      <span>Count: {count}</span>
-      <button onClick={increaseCount}>+</button>
-      <DisplayDate count={count} />
-    </div>
-  );
-}
-
-function DisplayDate({ count }) {
   const today = new Date();
-
-  function handleDate(count) {
-    const displayDate = new Date(today);
-    displayDate.setDate(today.getDate() + count);
-    return displayDate.toDateString();
-  }
+  today.setDate(today.getDate() + count);
 
   return (
     <div>
-      <span>
-        {count === 0
-          ? `Today is ${today.toDateString()}`
-          : count > 0
-          ? `${count} day${count === 1 ? "" : "s"} from today is ${handleDate(
-              count
-            )}`
-          : `${-count} day${-count === 1 ? "" : "s"} ago was ${handleDate(
-              count
-            )}`}
-      </span>
+      <div>
+        <button
+          onClick={() => setStep((s) => Math.max(1, s - 1))}
+          disabled={step === 1}
+        >
+          -
+        </button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+
+      <div>
+        <button onClick={() => setCount((s) => s - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((s) => s + step)}>+</button>
+      </div>
+
+      <div>
+        <span>
+          {count === 0
+            ? `Today's date is: `
+            : count > 0
+            ? `${count} day${count === 1 ? "" : "s"} from today is: `
+            : `${-count} day${-count === 1 ? "" : "s"} ago was: `}
+        </span>
+        <span>{today.toDateString()}</span>
+      </div>
     </div>
   );
 }
-
-export default App;
