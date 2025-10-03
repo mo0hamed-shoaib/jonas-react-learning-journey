6923f790840c55a10bd5b12121a698925d65af82
Mohamed Gamal <mohamed.g.shoaib@gmail.com>
2025-10-03 20:08:49 +0300

    user children prop for displaying accordion text

--- 04-steps/src/App.js
+++ 04-steps/src/App.js
@@ -40,9 +40,9 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
               <span>👈🏻</span>Previous
@@ -53,6 +53,15 @@ function Steps() {
           </div>
         </div>
       )}
+    </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
     </div>
   );
 }
